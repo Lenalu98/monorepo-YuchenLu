843da4f800953a76d1f047efa0078a36905ef5bd
Yuchen Lu <yuchenlu@YuchendeMacBook-Air.local>
2023-09-13 21:25:08 -0700

    Refactored rectangle class

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -5,13 +5,49 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
+from abc import ABC, abstractmethod
+
+# abstract class Shape
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
     def set_values(self, x, y):
-        self.width = x
-        self.height = y
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, width = 0, height = 0):
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
+
+    def set_values(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def get_width(self):
+        return self.__width
+
+    def set_width(self, width):
+        if width >= 0:
+            self.__width = width
+        else:
+            raise ValueError("Width must be a non-negative value")
+
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, height):
+        if height >= 0:
+            self.__height = height
+        else:
+            raise ValueError("Height must be a non-negative value")
 
 
 if __name__ == "__main__":
